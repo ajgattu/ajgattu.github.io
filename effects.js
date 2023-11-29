cd40ff345c78543a9bff6d73f62b183541365238
ajgattu <122301472+ajgattu@users.noreply.github.com>
2023-11-30 07:20:10 +1100

    motion effects js

--- effects.js
+++ effects.js
@@ -1,3 +1,46 @@
+/* website animation*/
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+/* Apply the animation to specific elements */
+body {
+  animation: fadeIn 1s ease-in-out;
+}
+
+/* Add a hover effect to portfolio items */
+.portfolio-item {
+  transition: transform 0.5s ease-in-out;
+}
+
+.portfolio-item:hover {
+  transform: scale(1.1);
+}
+
+/* Create a pulse effect for a call-to-action button */
+.cta-button {
+  animation: pulse 1.5s infinite;
+}
+
+@keyframes pulse {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.2);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
     // Wait for the DOM to be fully loaded before executing the script
 
